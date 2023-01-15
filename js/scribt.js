0a3d501b8fcffb3b24389b584559fd6a188422bd
unknown <agied.all@gmail.com>
2023-01-15 13:31:09 +0300

    portfolio

---
+++ js/scribt.js
@@ -0,0 +1,7 @@
+// ======================================== Typing Animation ================================
+var typed = new typed(".typing",{
+    strings:["Web Designer" , "Web Developer"],
+    typespeed:100,
+    backspeed:60,
+    loop:true
+}) 
